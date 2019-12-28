752fabd62df7310e810522bfaacc470427ea49ef
Tassilo <tassia710@gmail.com>
2019-12-28 19:23:03 +0100

    Core Class

---
+++ src/de/tassilo/Tassilo.java
@@ -0,0 +1,12 @@
+package de.tassilo;
+
+public final class Tassilo {
+	public static final int VERSION_MAJOR = 1;
+	public static final int VERSION_MINOR = 0;
+	public static final int VERSION_PATCH = 0;
+	public static final int VERSION_BUILD = 12;
+	
+	private Tassilo() {
+	}
+	
+}
