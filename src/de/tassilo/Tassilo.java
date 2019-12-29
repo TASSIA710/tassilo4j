0ea5fdef88b5bd02ea8c06cfe1ce38ee276dd7c4
Tassilo <tassia710@gmail.com>
2019-12-29 23:58:28 +0100

    Update Version

--- src/de/tassilo/Tassilo.java
+++ src/de/tassilo/Tassilo.java
@@ -4,7 +4,7 @@ public final class Tassilo {
 	public static final int VERSION_MAJOR = 1;
 	public static final int VERSION_MINOR = 0;
 	public static final int VERSION_PATCH = 0;
-	public static final int VERSION_BUILD = 13;
+	public static final int VERSION_BUILD = 15;
 	
 	private Tassilo() {
 	}
