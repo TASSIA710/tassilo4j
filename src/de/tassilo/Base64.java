73df6395b243e988c853671de52ff8762dba0446
Tassilo <tassia710@gmail.com>
2020-02-12 18:04:22 +0100

    Non-Base64 characters are now ignored

--- src/de/tassilo/Base64.java
+++ src/de/tassilo/Base64.java
@@ -127,19 +127,7 @@ public final class Base64 {
 	 * @throws IllegalArgumentException if the string contains non-Base64 characters
 	 */
 	public static final String decodeToString(String str, Charset ch) throws IllegalArgumentException {
-		if (str.indexOf('\n') != -1) {
-			String[] splits = str.split("\n");
-			for (int i = 0; i < splits.length; i++)
-				splits[i] = decodeToString(splits[i], ch);
-			return String.join("\n", splits);
-		}
-		if (str.indexOf('\r') != -1) {
-			String[] splits = str.split("\r");
-			for (int i = 0; i < splits.length; i++)
-				splits[i] = decodeToString(splits[i], ch);
-			return String.join("\r", splits);
-		}
-		return new String(decode(str), ch);
+		return new String(decode(str.replaceAll("[^A-Za-z0-9+/=]", "")), ch);
 	}
 
 	/**
