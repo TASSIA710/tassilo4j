855dc0c250f3b16bf888566255954af98e99d542
Tassilo <tassia710@gmail.com>
2020-07-09 18:10:51 +0200

    Update Server.java

--- src/main/java/net/tassia/net/server/Server.java
+++ src/main/java/net/tassia/net/server/Server.java
@@ -2,6 +2,8 @@ package net.tassia.net.server;
 
 import net.tassia.event.EventManager;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
@@ -14,9 +16,10 @@ public interface Server<C extends Client> {
 	Collection<C> getClients();
 
 	int getPort();
+	InetAddress getBind();
 
-	void start();
-	void stop();
+	void start() throws IOException;
+	void stop() throws IOException;
 	boolean isReady();
 	boolean isClosed();
 
