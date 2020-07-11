a5cc6593ddbf25d4c1ae5987a2d6c0790f745aec
Tassilo <tassia710@gmail.com>
2020-07-11 17:35:26 +0200

    Create LightHttpRequest.java

---
+++ src/main/java/net/tassia/net/server/light/http/LightHttpRequest.java
@@ -0,0 +1,57 @@
+package net.tassia.net.server.light.http;
+
+import net.tassia.net.server.http.HttpMethod;
+import net.tassia.net.server.http.HttpProtocol;
+import net.tassia.net.server.http.HttpRequest;
+import net.tassia.net.server.light.LightClient;
+
+import java.util.Map;
+
+public class LightHttpRequest implements HttpRequest {
+    private final HttpProtocol protocolVersion;
+    private final HttpMethod method;
+    private final Map<String, String> headers;
+    private final byte[] content;
+    private final LightHttpServer server;
+    private final LightClient client;
+
+    public LightHttpRequest(HttpProtocol protocolVersion, HttpMethod method, Map<String, String> headers,
+                            byte[] content, LightHttpServer server, LightClient client) {
+        this.protocolVersion = protocolVersion;
+        this.method = method;
+        this.headers = headers;
+        this.content = content;
+        this.server = server;
+        this.client = client;
+    }
+
+    @Override
+    public HttpProtocol getProtocolVersion() {
+        return protocolVersion;
+    }
+
+    @Override
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    @Override
+    public byte[] getContent() {
+        return content;
+    }
+
+    @Override
+    public LightHttpServer getServer() {
+        return server;
+    }
+
+    @Override
+    public LightClient getClient() {
+        return client;
+    }
+}
