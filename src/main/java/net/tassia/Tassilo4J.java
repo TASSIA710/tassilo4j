d085dfab88e2e10f6d9e920c47411080c6e73f13
Tassilo <tassia710@gmail.com>
2020-07-12 13:40:08 +0200

    Added dumpHex

--- src/main/java/net/tassia/Tassilo4J.java
+++ src/main/java/net/tassia/Tassilo4J.java
@@ -1,4 +1,19 @@
 package net.tassia;
 
+import java.io.PrintStream;
+
 public class Tassilo4J {
+
+	public static void dumpHex(byte[] array) {
+		dumpHex(array, System.out);
+	}
+
+	public static void dumpHex(byte[] array, PrintStream out) {
+		for (byte b : array) {
+			out.print(String.format("%02X", b));
+			out.print(" ");
+		}
+		out.println();
+	}
+
 }
