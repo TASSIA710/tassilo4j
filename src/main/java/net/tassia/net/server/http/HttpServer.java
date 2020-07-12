6278ac7ce2aeccaa8af96096c2770f355f85dd46
Tassilo <tassia710@gmail.com>
2020-07-12 14:01:08 +0200

    Update HttpServer.java

--- src/main/java/net/tassia/net/server/http/HttpServer.java
+++ src/main/java/net/tassia/net/server/http/HttpServer.java
@@ -9,7 +9,7 @@ import java.util.function.Function;
 public interface HttpServer<C extends Client> extends Server<C> {
 
     int DEFAULT_PORT = 80;
-    
+
     void addRouteStatic(String path, HttpCallback callback, HttpMethod...methods);
     void addRoute(String regex, HttpCallback callback, HttpMethod...methods);
 
